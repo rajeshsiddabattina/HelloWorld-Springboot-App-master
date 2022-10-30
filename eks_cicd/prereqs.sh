e8befcea8dfbea3e4f52823cdc83925d56042219
rajeshsiddabattina <97787724+rajeshsiddabattina@users.noreply.github.com>
2022-10-30 23:33:10 +0530

    Update prereqs.sh

--- eks_cicd/prereqs.sh
+++ eks_cicd/prereqs.sh
@@ -41,23 +41,23 @@ else
 fi
 
 # eksctl
-if ! type eksctl >/dev/null 2>&1; then
-	curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-	mv /tmp/eksctl $HOME/bin
-	echo 'eksctl installed'
-else
-	echo 'eksctl already installed'
-fi
+#if ! type eksctl >/dev/null 2>&1; then
+#	curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+#	mv /tmp/eksctl $HOME/bin
+#	echo 'eksctl installed'
+#else
+#	echo 'eksctl already installed'
+#fi
 
 # kubectx/kubens
-if ! type kubectx >/dev/null 2>&1; then
-	git clone https://github.com/ahmetb/kubectx /opt/kubectx
-	ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-	ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-	echo 'kubectx installed'
-else
-	echo 'kubectx already installed'
-fi
-
+#if ! type kubectx >/dev/null 2>&1; then
+#	git clone https://github.com/ahmetb/kubectx /opt/kubectx
+#	ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+#	ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+#	echo 'kubectx installed'
+#else
+#	echo 'kubectx already installed'
+#fi
+echo 'shell script end of tasks completed '
 # Test if AWS credentials exist
 aws sts get-caller-identity
